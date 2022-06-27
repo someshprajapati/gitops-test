e301e929efbfe71aa356047b71e8690fb6908e7c
soprajap <someshp@juniper.net>
2022-06-27 14:56:26 +0530

    Added the version file

--- version_check.sh
+++ version_check.sh
@@ -2,8 +2,10 @@
 
 version_dir="staging"
 
+# Get the file list from the version directory
 declare -a file_list=$(ls -l $version_dir/ | sed 1d | awk -F" " '{print $9}' | awk -F"." '{print $1}' | sort | uniq)
-#declare -a channel_list=("alpha" "beta" "stable")
+
+# Get the channel list from the files
 declare -a channel_list=$(ls -l $version_dir/ | sed 1d | awk -F" " '{print $9}' | awk -F"." '{print $2}' | sort | uniq)
 
 for file_name in ${file_list[@]}
@@ -18,9 +20,9 @@ do
         grep -w "$default_version" $versions_file_name
         if [ "$?" = "0" ]
         then
-            echo "version found for file: $file_name.$channel_name.default"
+            echo "Version: [$default_version] found in file: $versions_file_name"
         else
-            echo "version not found for file: $file_name.$channel_name.default"
+            echo "Version: [$default_version] not found in file: $versions_file_name"
         fi
     done
 done
